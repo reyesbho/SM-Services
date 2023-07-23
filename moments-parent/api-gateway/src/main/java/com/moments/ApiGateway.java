cc9ef7f6e6ef53be71710724aaec1d08fe24c083
Reyes <reyesbho@gmail.com>
2023-07-23 15:14:24 -0600

    advance apu gateway and spring security

--- moments-parent/api-gateway/src/main/java/com/moments/ApiGateway.java
+++ moments-parent/api-gateway/src/main/java/com/moments/ApiGateway.java
@@ -2,8 +2,9 @@ package com.moments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class ApiGateway {
     public static void main(String[] args) {
         SpringApplication.run(ApiGateway.class, args);
